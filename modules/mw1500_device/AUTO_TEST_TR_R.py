fd9f876c29c6a25d63b7c9c91aff9adf0bb9a005
renjie.zhang <renjie.zhang@tianheng-uestc.com>
2020-03-09 10:55:46 +0800

    fixed test resource test_ip issue

--- modules/mw1500_device/AUTO_TEST_TR_R.py
+++ modules/mw1500_device/AUTO_TEST_TR_R.py
@@ -36,8 +36,8 @@ class AUTO_TEST_TR_R(QDialog, Ui_Dialog):
         self.demo = True
         
     def initUi(self,mConfig):
-        self.addr_sg= mConfig.test_ip[0]
-        self.addr_sa= mConfig.test_ip[1]
+        self.addr_sg= "192.168.1.141"
+        self.addr_sa= "192.168.1.142"
         self.freq_H= str(self.comboBox_freq_H.currentText())
         self.freq_M= str(self.comboBox_freq_M.currentText())
         self.freq_L= str(self.comboBox_freq_L.currentText())
